15844314fcb7ce4ea5fedc4440d0fb1261010767
emanoelk <khemanoel@gmail.com>
2016-11-29 21:32:11 -0800

    check

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,14 +4,18 @@ myApp.controller('AppCtrl', ['$scope', '$http',
 	
 var refresh = function() {
   $http.get('/personalinfo').success(function(response) {
-    
+    if(($scope.schools = response) != null){
 	$scope.schools = response;
 	$scope.school = "";
+	};
+	if(($scope.skills = response) != null){
 	$scope.skills = response;
 	$scope.skill = "";	
+	};
+	if(($scope.projects = response) != null){
 	$scope.projects = response;
 	$scope.project = "";
-	
+	};
 
   });
 };
